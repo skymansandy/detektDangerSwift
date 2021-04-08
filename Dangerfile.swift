b045f978bba830146c978d5928efa3c426195a6b
Sandesh <sandesh.baliga@compass.com>
2021-04-08 20:41:19 +0530

    Added swiftlint to pod

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,5 @@
 import Danger 
 let danger = Danger()
 
-danger.message("Hello from Danger")
-
 SwiftLint.lint(inline: true, configFile: ".swiftlint.yml")
 
---
+++ Pods/Target Support Files/Pods-detektDangerSwiftTests/Pods-detektDangerSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_detektDangerSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_detektDangerSwiftTests
+@end
